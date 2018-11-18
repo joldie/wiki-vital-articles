a4d3204eb8cba38535098ce3de0b1ae50a02b66b
joldie <j.oldridge@mykolab.com>
2018-11-18 09:58:36 +0100

    Added CORS support.

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const vitalArticles = require("../main.js");
 
+// Enable CORS for all origins
+app.use(cors());
+
 app.get("/", async function(req, res) {
+  // Get list of articles by scraping Wikipedia webpage
   const articles = await vitalArticles();
+  // Set headers and send JSON object containing all articles
   res.setHeader("Content-Type", "application/json");
   res.send(articles);
 });
